8d1914dc54fd1ebbdd298ffcb652def7e9a7f73a
Janek Brandt <brandt.janek@gmail.com>
2017-07-18 15:16:22 -0700

    add gesture listener to TaskWheel class to listen for fling gestures and calculate task to land on and number of extra rotations to make

--- app/src/main/java/com/jbnm/homehero/ui/taskwheel/TaskWheel.java
+++ app/src/main/java/com/jbnm/homehero/ui/taskwheel/TaskWheel.java
@@ -7,6 +7,9 @@ import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.jbnm.homehero.R;
@@ -25,14 +28,14 @@ public class TaskWheel extends View {
 
     private List<Task> taskItems = new ArrayList<>();
 
+    private GestureDetector gestureDetector;
+
     public TaskWheel(Context context) {
         this(context, null);
     }
-
     public TaskWheel(Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, 0);
     }
-
     public TaskWheel(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         backgroundColor = ContextCompat.getColor(context, R.color.taskWheelBackground);
@@ -48,6 +51,8 @@ public class TaskWheel extends View {
         taskPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         taskPaint.setStyle(Paint.Style.FILL);
         taskPaint.setColor(taskWheelColorOne);
+
+        gestureDetector = new GestureDetector(TaskWheel.this.getContext(), new GestureListener());
     }
 
     @Override
@@ -68,12 +73,6 @@ public class TaskWheel extends View {
         arc.set(getPaddingLeft(), getPaddingTop(), w - getPaddingRight(), h - getPaddingBottom());
     }
 
-    public void addTasks(List<Task> tasks) {
-        taskItems = tasks;
-        taskAngle = 360f / taskItems.size();
-        invalidate();
-    }
-
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawCircle(centerX, centerY, radius, circlePaint);
@@ -92,4 +91,59 @@ public class TaskWheel extends View {
         }
         canvas.drawArc(arc, startAngle, taskAngle, true, taskPaint);
     }
+
+    public void addTasks(List<Task> tasks) {
+        taskItems = tasks;
+        taskAngle = 360f / taskItems.size();
+        invalidate();
+    }
+
+    private void rotateToTask(int taskSection, int extraRotations) {
+        Log.d("test", "section: " + taskSection + " rotations: " + extraRotations);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        boolean result = gestureDetector.onTouchEvent(event);
+        if (!result) {
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    private class GestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            float flingStrength = vectorToScalarScroll(velocityX, velocityY, e2.getX() - centerX, e2.getY() - centerY);
+            float angle = flingStrength % 360;
+            int section = (int)Math.floor(Math.abs(angle) / taskAngle);
+            int rotations = (int)Math.ceil(Math.abs(flingStrength) / 360);
+            rotateToTask(section, rotations);
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+    }
+
+    private static float vectorToScalarScroll(float dx, float dy, float x, float y) {
+        // get the length of the vector
+        float l = (float) Math.sqrt(dx * dx + dy * dy);
+
+        // decide if the scalar should be negative or positive by finding
+        // the dot product of the vector perpendicular to (x,y).
+        float crossX = -y;
+        float crossY = x;
+
+        float dot = (crossX * dx + crossY * dy);
+        float sign = Math.signum(dot);
+
+        return l * sign;
+    }
+
 }
