42783d70aaca634e08bcdbbb6546a55ccd9eb4ee
Janek Brandt <brandt.janek@gmail.com>
2017-07-27 14:47:58 -0700

    change tutorial arrow image, add listener to task wheel for start of spin. hide tutorial on task wheel spin. Add pulse animation to tutorial arrow

--- app/src/main/java/com/jbnm/homehero/ui/taskpicker/TaskPickerActivity.kt
+++ app/src/main/java/com/jbnm/homehero/ui/taskpicker/TaskPickerActivity.kt
@@ -3,6 +3,7 @@ package com.jbnm.homehero.ui.taskpicker
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -13,17 +14,18 @@ import com.jbnm.homehero.ui.base.BaseActivity
 import com.jbnm.homehero.ui.goal.GoalActivity
 
 import kotlinx.android.synthetic.main.activity_task_picker.*
+import kotlinx.android.synthetic.main.task_picker_result.*
 
 class TaskPickerActivity : BaseActivity(), TaskPickerContract.MvpView {
 
     lateinit var presenter: TaskPickerContract.Presenter
+    var tutorialAnimation: ObjectAnimator? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_picker)
 
         presenter = TaskPickerPresenter(this)
-        tutorialView.visibility = View.GONE
         result.visibility = View.GONE
 
         taskSelector.setOnTaskSelectListener { presenter.taskSelected(it) }
@@ -84,11 +86,20 @@ class TaskPickerActivity : BaseActivity(), TaskPickerContract.MvpView {
     }
 
     override fun showTutorial() {
-        tutorialView.visibility = View.VISIBLE
-        tutorialView.setOnClickListener { presenter.tutorialClick() }
+        tutorialImageView.visibility = View.VISIBLE
+        tutorialAnimation = ObjectAnimator.ofPropertyValuesHolder(tutorialImageView,
+                PropertyValuesHolder.ofFloat("scaleX", 0.8f),
+                PropertyValuesHolder.ofFloat("scaleY", 0.8f))
+        tutorialAnimation?.duration = 500
+        tutorialAnimation?.repeatCount = ObjectAnimator.INFINITE
+        tutorialAnimation?.repeatMode = ObjectAnimator.REVERSE
+        tutorialAnimation?.start()
+
+        taskSelector.setOnSpinStartListener { presenter.tutorialClick() }
     }
 
     override fun hideTutorial() {
-        tutorialView.visibility = View.GONE
+        tutorialImageView.visibility = View.GONE
+        tutorialAnimation?.end()
     }
 }
