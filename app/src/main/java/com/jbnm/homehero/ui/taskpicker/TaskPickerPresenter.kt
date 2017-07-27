2d9f5a1a0fd830ca6f4f30947098fd6d4d474112
Janek Brandt <brandt.janek@gmail.com>
2017-07-27 10:39:47 -0700

    refactor TaskPicker to use 2 subscriptions instead of 3 and take advantage of the subscribe method returning a disposable when using lambdas to remove need for observer object.

--- app/src/main/java/com/jbnm/homehero/ui/taskpicker/TaskPickerPresenter.kt
+++ app/src/main/java/com/jbnm/homehero/ui/taskpicker/TaskPickerPresenter.kt
@@ -3,62 +3,48 @@ package com.jbnm.homehero.ui.taskpicker
 import com.jbnm.homehero.data.model.Child
 import com.jbnm.homehero.data.model.Task
 import com.jbnm.homehero.data.remote.DataManager
-import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
 
 class TaskPickerPresenter(val mvpView: TaskPickerContract.MvpView) : TaskPickerContract.Presenter {
-    var dataManager: DataManager? = null
-    var disposable: CompositeDisposable? = null
+    val dataManager: DataManager = DataManager()
+    val disposable: CompositeDisposable = CompositeDisposable()
 
     // Temporary, will be passed in to from activity or retrieved based on logged in user
     val childId = "-Kpulp2slG8NxvjE3l0u"
-    var child: Child? = null
+    lateinit var child: Child
     init {
-        dataManager = DataManager()
-        disposable = CompositeDisposable()
         mvpView.showLoading()
 
-        dataManager?.getChild(childId)
-                ?.observeOn(AndroidSchedulers.mainThread())
-                ?.subscribe(getObserver { childResult ->
-                    child = childResult
-                    loadTasks()
-                })
-    }
-
-    fun loadTasks() {
-        dataManager?.getAllTasksFromList(child?.tasks?.keys?.toList())
-                ?.observeOn(AndroidSchedulers.mainThread())
-                ?.subscribe(getObserver {tasks -> processTasks(tasks)})
-    }
-
-    inline fun <T> getObserver(crossinline body: (T) -> Unit) = object : Observer<T>{
-        override fun onSubscribe(d: Disposable) { disposable?.add(d) }
-
-        override fun onComplete() {}
-
-        override fun onNext(t: T) { body(t) }
-
-        override fun onError(e: Throwable) { e.printStackTrace() }
+        disposable.add(dataManager.getChild(childId)
+                .doOnNext { childResult -> child = childResult }
+                .switchMap { childResult -> dataManager.getAllTasksFromList(childResult.tasks.keys.toList()) }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ processTasks(it) },
+                        { processError(it) }))
     }
 
     fun processTasks(tasks: List<Task>) {
-        mvpView.hideLoading()
         mvpView.addTasks(tasks)
+        mvpView.hideLoading()
         mvpView.showTasksCompleted(tasks.filter { !it.availableForSelection() }.size, tasks.size)
     }
 
+    fun processError(error: Throwable) {
+        error.printStackTrace()
+    }
+
     override fun detach() {
-        disposable?.clear()
+        disposable.clear()
     }
 
     override fun taskSelected(task: Task) {
-        child?.currentTask = task.id
-        dataManager?.updateChild(child)
-                ?.observeOn(AndroidSchedulers.mainThread())
-                ?.subscribe(getObserver { mvpView.showSelectedTask(task) })
+        child.currentTask = task.id
+
+        disposable.add(dataManager.updateChild(child)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ mvpView.showSelectedTask(task) },
+                        { processError(it) }))
     }
 
     override fun goalButtonClick() {
