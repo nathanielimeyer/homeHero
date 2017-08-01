973809e6fd90d42b31801a41149070b9485ab6f1
Janek Brandt <brandt.janek@gmail.com>
2017-08-01 12:53:53 -0700

    add baseFragment that implements baseMvpView interface. taskreview and settings fragments extends basefragment

--- app/src/main/java/com/jbnm/homehero/ui/parent/settings/SettingsContract.java
+++ app/src/main/java/com/jbnm/homehero/ui/parent/settings/SettingsContract.java
@@ -1,8 +1,17 @@
 package com.jbnm.homehero.ui.parent.settings;
 
+import com.jbnm.homehero.ui.base.BaseMvpView;
+import com.jbnm.homehero.ui.base.BasePresenter;
+
 /**
  * Created by janek on 8/1/17.
  */
 
 public class SettingsContract {
+    interface Presenter extends BasePresenter {
+
+    }
+    interface MvpView extends BaseMvpView {
+
+    }
 }
--- app/src/main/java/com/jbnm/homehero/ui/parent/settings/SettingsFragment.java
+++ app/src/main/java/com/jbnm/homehero/ui/parent/settings/SettingsFragment.java
@@ -2,32 +2,57 @@ package com.jbnm.homehero.ui.parent.settings;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.jbnm.homehero.R;
+import com.jbnm.homehero.ui.base.BaseFragment;
 
-public class SettingsFragment extends Fragment {
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
+public class SettingsFragment extends BaseFragment implements SettingsContract.MvpView {
+    private SettingsContract.Presenter presenter;
+    private Unbinder unbinder;
 
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
+    public SettingsFragment() {}
 
     public static SettingsFragment newInstance() {
         SettingsFragment fragment = new SettingsFragment();
-        // add to bundle here
         return fragment;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        presenter = new SettingsPresenter(this);
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onDestroy() {
+        super.onDestroy();
+        presenter.detach();
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        unbinder = ButterKnife.bind(this, view);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @Override
+    protected int getFragmentLayout() {
+        return R.layout.fragment_settings;
+    }
+
 
 }
