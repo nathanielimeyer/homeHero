8f967c1261f175125f7c88922603ff3e772e3ded
Janek Brandt <brandt.janek@gmail.com>
2017-08-01 17:05:53 -0700

    add recyclerview adapter to show current tasks grouped by status

--- app/src/main/java/com/jbnm/homehero/ui/parent/taskreview/TaskReviewFragment.java
+++ app/src/main/java/com/jbnm/homehero/ui/parent/taskreview/TaskReviewFragment.java
@@ -2,15 +2,24 @@ package com.jbnm.homehero.ui.parent.taskreview;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.jbnm.homehero.R;
+import com.jbnm.homehero.data.model.Task;
 import com.jbnm.homehero.ui.base.BaseFragment;
 
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class TaskReviewFragment extends BaseFragment implements TaskReviewContract.MvpView {
+    @BindView(R.id.taskReviewRecyclerView) RecyclerView taskReviewRecyclerView;
+
     private TaskReviewContract.Presenter presenter;
     private Unbinder unbinder;
 
@@ -37,6 +46,7 @@ public class TaskReviewFragment extends BaseFragment implements TaskReviewContra
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         unbinder = ButterKnife.bind(this, view);
+        presenter.loadTasks("-Kq5YlmM3saCunGh6Jr_");
     }
 
     @Override
@@ -50,4 +60,12 @@ public class TaskReviewFragment extends BaseFragment implements TaskReviewContra
         return R.layout.fragment_task_review;
     }
 
+    @Override
+    public void showTasks(List<Object> tasks) {
+        taskReviewRecyclerView.setAdapter(new TaskReviewAdapter(tasks));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        taskReviewRecyclerView.setLayoutManager(layoutManager);
+        taskReviewRecyclerView.setHasFixedSize(true);
+        taskReviewRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), layoutManager.getOrientation()));
+    }
 }
--- app/src/main/java/com/jbnm/homehero/ui/parent/taskreview/TaskReviewPresenter.java
+++ app/src/main/java/com/jbnm/homehero/ui/parent/taskreview/TaskReviewPresenter.java
@@ -1,17 +1,117 @@
 package com.jbnm.homehero.ui.parent.taskreview;
 
+import android.util.Log;
+
+import com.jbnm.homehero.data.model.Child;
+import com.jbnm.homehero.data.model.Task;
+import com.jbnm.homehero.data.remote.DataManager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.BiConsumer;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.observables.GroupedObservable;
+import io.reactivex.observers.DisposableObserver;
+
 /**
  * Created by janek on 8/1/17.
  */
 
 public class TaskReviewPresenter implements TaskReviewContract.Presenter {
     private TaskReviewContract.MvpView mvpView;
+    private CompositeDisposable disposable = new CompositeDisposable();
+    private DataManager dataManager = new DataManager();
+    private Child child;
 
     public TaskReviewPresenter(TaskReviewContract.MvpView mvpView) {
         this.mvpView = mvpView;
     }
 
     public void detach() {
+        disposable.clear();
+    }
 
+    @Override
+    public void loadTasks(String childId) {
+        mvpView.showLoading();
+        disposable.add(dataManager.getChild(childId)
+                .doOnNext(new Consumer<Child>() {
+                    @Override public void accept(Child childResult) throws Exception {
+                        child = childResult;
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableObserver<Child>() {
+                    @Override public void onNext(Child child) { processResult(child); }
+                    @Override public void onError(Throwable e) { processError(e); }
+                    @Override public void onComplete() {}
+                }));
+    }
+
+    @Override
+    public void handleTaskMarkedComplete(String taskId) {
+
+    }
+
+    @Override
+    public void handleTaskMarkedIncomplete(String taskId) {
+
+    }
+
+    private void processResult(Child child) {
+        List<Object> items = Observable.fromIterable(child.getTasks().values())
+                .collect(new Callable<Map<String, List<Task>>>() {
+                    @Override public Map<String, List<Task>> call() throws Exception {
+                        return new HashMap<>();
+                    }
+                }, new BiConsumer<Map<String, List<Task>>, Task>() {
+                    @Override public void accept(Map<String, List<Task>> map, Task task) throws Exception {
+                        String taskStatus = taskStatus(task);
+                        if (map.containsKey(taskStatus)) {
+                            map.get(taskStatus).add(task);
+                        } else {
+                            map.put(taskStatus, new ArrayList<Task>());
+                            map.get(taskStatus).add(task);
+                        }
+                    }
+                }).map(new Function<Map<String,List<Task>>, List<Object>>() {
+                    @Override public List<Object> apply(Map<String, List<Task>> stringListMap) throws Exception {
+                        List<Object> items = new ArrayList<>();
+                        for (String key : stringListMap.keySet()) {
+                            items.add(key);
+                            items.addAll(stringListMap.get(key));
+                        }
+                        return items;
+                    }
+                }).blockingGet();
+
+        mvpView.showTasks(items);
+        mvpView.hideLoading();
+    }
+
+    private String taskStatus(Task task) {
+        if (task.pendingApproval()) {
+            return "Pending";
+        } else if (task.availableForSelection()) {
+            return "Incomplete";
+        } else {
+            return "Complete";
+        }
+    }
+
+    private void processError(Throwable e) {
+        mvpView.hideLoading();
+        e.printStackTrace();
+        mvpView.showError(e.getMessage());
     }
 }
