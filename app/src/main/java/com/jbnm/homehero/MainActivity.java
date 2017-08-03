9cf509f2ab274ab94ea881d267afbf8c7d6cfa1d
Janek Brandt <brandt.janek@gmail.com>
2017-08-03 11:02:18 -0700

    remove setup code from parent activity and remove unused imports from main and base activities

--- app/src/main/java/com/jbnm/homehero/MainActivity.java
+++ app/src/main/java/com/jbnm/homehero/MainActivity.java
@@ -2,26 +2,14 @@ package com.jbnm.homehero;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
-import com.jbnm.homehero.data.model.Child;
-import com.jbnm.homehero.data.model.Parent;
-import com.jbnm.homehero.data.model.Reward;
-import com.jbnm.homehero.data.model.Task;
-import com.jbnm.homehero.data.remote.DataManager;
 import com.jbnm.homehero.ui.base.BaseActivity;
 import com.jbnm.homehero.ui.goal.GoalActivity;
 import com.jbnm.homehero.ui.taskEdit.TaskEditorActivity;
 import com.jbnm.homehero.ui.taskpicker.TaskPickerActivity;
 
-import java.util.Arrays;
-import java.util.List;
-
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends BaseActivity {
 
