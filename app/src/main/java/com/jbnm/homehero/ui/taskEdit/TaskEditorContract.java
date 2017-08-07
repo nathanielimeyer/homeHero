de9118831a4dd8afd44144113d432a320bff28c0
Nathaniel Meyer <nathanielimeyer@gmail.com>
2017-08-07 00:49:57 -0700

    Add step a better way; support new tasks

--- app/src/main/java/com/jbnm/homehero/ui/taskEdit/TaskEditorContract.java
+++ app/src/main/java/com/jbnm/homehero/ui/taskEdit/TaskEditorContract.java
@@ -14,7 +14,7 @@ public class TaskEditorContract {
 //        void saveChildData(String description, String icon, List<String> instructions);
         void saveChildData(String description);
         void loadChildAndTask(String childId, String taskId);
-//        void addStepsButtonClicked();
+        void addStepsButtonClicked();
     }
     interface MvpView extends BaseMvpView {
         void setDescription(String description);
