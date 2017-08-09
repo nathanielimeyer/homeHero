532eaf5b8c03201341366bfb44b9b8e3d94e5e1a
Nathaniel Meyer <nathanielimeyer@gmail.com>
2017-08-08 23:34:08 -0700

    Add ParentTaskListActivity

--- app/src/main/java/com/jbnm/homehero/ui/parent/taskList/ParentTaskListActivity.java
+++ app/src/main/java/com/jbnm/homehero/ui/parent/taskList/ParentTaskListActivity.java
@@ -3,14 +3,21 @@ package com.jbnm.homehero.ui.parent.taskList;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.jbnm.homehero.Constants;
 import com.jbnm.homehero.R;
+import com.jbnm.homehero.data.model.Task;
 import com.jbnm.homehero.ui.base.BaseActivity;
 import com.jbnm.homehero.ui.taskEdit.TaskEditorActivity;
 
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -18,8 +25,13 @@ import butterknife.OnClick;
  * Created by nathanielmeyer on 8/7/17.
  */
 
-public class ParentTaskListActivity extends BaseActivity implements ParentTaskListContract.MvpView {
+public class ParentTaskListActivity extends BaseActivity implements ParentTaskListContract.MvpView, ParentTaskClickListener {
+    @BindView(R.id.taskListRecyclerView) RecyclerView taskListRecyclerView;
+    @BindView(R.id.addTaskButton) Button addTaskButton;
+    @BindView(R.id.buttonBackToSettings) Button backButton;
 
+    List<Task> tasks;
+    String childId;
     private Context context = this;
     private ParentTaskListContract.Presenter presenter;
 
@@ -35,28 +47,56 @@ public class ParentTaskListActivity extends BaseActivity implements ParentTaskLi
         setContentView(R.layout.activity_parent_task_list);
         ButterKnife.bind(this);
 
-        String childId = getIntent().getStringExtra("childId");
+        childId = getIntent().getStringExtra("childId");
 
         presenter = new ParentTaskListPresenter(this);
-        presenter.loadChild(childId);
+        presenter.loadTasks(childId);
 
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.addTaskButtonClicked();
+            }
+        });
 
-    }
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-    @OnClick(R.id.buttonEditTask)
-    public void editTaskButton() {
-        Intent intent = new Intent(ParentTaskListActivity.this, TaskEditorActivity.class);
-        intent.putExtra(Constants.CHILD_INTENT_KEY, Constants.CHILDID);
-        startActivity(intent);
+            }
+        });
     }
 
     @Override
-    public void setDescriptions(List<String> descriptions) {
-
+    public void listTasks(List<Task> tasks) {
+        this.tasks = tasks;
+        ParentTaskListAdapter parentTaskListAdapter = new ParentTaskListAdapter(tasks);
+        parentTaskListAdapter.setParentTaskClickListener(this);
+        taskListRecyclerView.setAdapter(parentTaskListAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        taskListRecyclerView.setLayoutManager(layoutManager);
+        taskListRecyclerView.setHasFixedSize(true);
+        taskListRecyclerView.addItemDecoration(new DividerItemDecoration(this, layoutManager.getOrientation()));
     }
 
     @Override
-    public void loadIcons(List<String> icons) {
+    public void editTaskIntent(String childId, String taskId) {
+    }
 
+    @Override
+    public void addTaskIntent(String childId) {
+        startActivity(TaskEditorActivity.createIntent(this, childId, "newTask"));
+    }
+
+    @Override
+    public void onEditTask(String taskId) {
+
+        startActivity(TaskEditorActivity.createIntent(this, childId, taskId));
+    }
+
+    @Override
+    public void onDeleteTask(Task task) {
+        tasks.remove(task);
+        presenter.saveTasks(tasks);
     }
 }
